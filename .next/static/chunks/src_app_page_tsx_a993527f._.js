2d3cea3ca51016c2cb7728d814b896c3557cbc64
lumoonm <lumoonm@gmail.com>
2025-07-20 16:08:02 -0300

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1753038400029

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_fc90873a._.js",
+  "static/chunks/src_e723e2ab._.js"
+],
+    source: "dynamic"
+});
